5f8ce9075cecee97ea7ea5c7b394da6fcf7d85e4
zhaoh <yanyan.zh@alibaba-inc.com>
2026-05-11 11:45:26 +0800

    resource/alicloud_api_gateway_api: Fixed backend_id read issue; resource/alicloud_api_gateway_vpc_access: Added vpc_access_id

--- alicloud/resource_alicloud_api_gateway_api.go
+++ alicloud/resource_alicloud_api_gateway_api.go
@@ -352,6 +352,15 @@ func resourceAliyunApigatewayApi() *schema.Resource {
 				Optional: true,
 				Computed: true,
 			},
+			"backend_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"backend_enabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
 		},
 	}
 }
@@ -456,6 +465,12 @@ func resourceAliyunApigatewayApiRead(d *schema.ResourceData, meta interface{}) e
 
 	d.Set("system_parameters", convertApiGatewayApiSystemParamsResponse(objectRaw["SystemParameters"]))
 
+	if backendConfig, ok := objectRaw["BackendConfig"].(map[string]interface{}); ok {
+		if v, ok := backendConfig["BackendId"].(string); ok && v != "" {
+			d.Set("backend_id", v)
+		}
+	}
+
 	return nil
 }
 
@@ -497,7 +512,7 @@ func resourceAliyunApigatewayApiUpdate(d *schema.ResourceData, meta interface{})
 	}
 	request.RequestConfig = paramConfig
 
-	if d.HasChanges("service_type", "http_service_config", "http_vpc_service_config", "mock_service_config", "fc_service_config") {
+	if d.HasChanges("service_type", "http_service_config", "http_vpc_service_config", "mock_service_config", "fc_service_config", "backend_id", "backend_enabled") {
 		update = true
 	}
 	serviceConfig, err := serviceConfigToJsonStr(d)
@@ -523,6 +538,11 @@ func resourceAliyunApigatewayApiUpdate(d *schema.ResourceData, meta interface{})
 		request.Visibility = Visibility
 		request.AllowSignatureMethod = AllowSignatureMethod
 		request.WebSocketApiType = WebSocketApiType
+
+		if v, ok := d.GetOk("backend_id"); ok && v.(string) != "" {
+			request.QueryParams["BackendId"] = v.(string)
+		}
+		request.QueryParams["BackendEnable"] = strconv.FormatBool(d.Get("backend_enabled").(bool))
 
 		raw, err := client.WithCloudApiClient(func(cloudApiClient *cloudapi.Client) (interface{}, error) {
 			return cloudApiClient.ModifyApi(request)
@@ -645,6 +665,11 @@ func buildAliyunApiArgs(d *schema.ResourceData, meta interface{}) (*cloudapi.Cre
 	request.Visibility = Visibility
 	request.AllowSignatureMethod = AllowSignatureMethod
 	request.WebSocketApiType = WebSocketApiType
+
+	if v, ok := d.GetOk("backend_id"); ok && v.(string) != "" {
+		request.QueryParams["BackendId"] = v.(string)
+	}
+	request.QueryParams["BackendEnable"] = strconv.FormatBool(d.Get("backend_enabled").(bool))
 
 	return request, WrapError(err)
 }
